f4e855513e0f70ce19f91f1b1090c45fa5f7fa27
Mayra Alvarez Caudillo <malvarez322@accitesz.com>
2020-02-12 12:18:52 -0600

    Creacion de la interfaz
    Editar deducciones

--- AgroNegocio_RH_ERP_ISC_8A/Interfaces/Deducciones_Editar.cs
+++ AgroNegocio_RH_ERP_ISC_8A/Interfaces/Deducciones_Editar.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AgroNegocio_RH_ERP_ISC_8A.Interfaces
 {
-    public partial class Form1 : Form
+    public partial class Deducciones_Editar : Form
     {
-        public Form1()
+        public Deducciones_Editar()
         {
             InitializeComponent();
         }
