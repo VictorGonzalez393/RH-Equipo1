3ed9aa310dd6a043673e4e1344e140234f5d060c
Liz <lvilla415@accitesz.com>
2020-02-12 18:38:36 -0600

    Prueba1

--- AgroNegocio_RH_ERP_ISC_8A/Interfaces/Percepciones_editar.cs
+++ AgroNegocio_RH_ERP_ISC_8A/Interfaces/Percepciones_editar.cs
@@ -21,5 +21,50 @@ namespace AgroNegocio_RH_ERP_ISC_8A.Interfaces
         {
 
         }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void id_percepcion_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nombre_percepcion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void descriocion_percepcion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
