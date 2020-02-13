c6075d72b883918ef03c06a9bb8d16953f7e4bb9
Mayra Alvarez Caudillo <malvarez322@accitesz.com>
2020-02-12 21:40:10 -0600

    Creacion interfaz Deducciones
    Interfaz principal de Deducciones

---
+++ AgroNegocio_RH_ERP_ISC_8A/Interfaces/Deducciones_GUI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgroNegocio_RH_ERP_ISC_8A.Interfaces
+{
+    public partial class Deducciones_GUI : Form
+    {
+        public Deducciones_GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
