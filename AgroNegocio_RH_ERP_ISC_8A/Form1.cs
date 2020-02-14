05a6fff1a045f1db20572aff578cb431e9857175
Liz <lvilla415@accitesz.com>
2020-02-13 22:06:08 -0600

    Revert "BD"
    This reverts commit 8f58bf5a28a73e5a54e1eeb209651790affc01b9, reversing
    changes made to b47fe18e16979946cbb847bd49dd225117bb2edd.

--- AgroNegocio_RH_ERP_ISC_8A/Form1.cs
+++ AgroNegocio_RH_ERP_ISC_8A/Form1.cs
@@ -8,23 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace AgroNegocio_RH_ERP_ISC_8A
 {
     public partial class Form1 : Form
     {
-        private Datos.Conexion cn;
         public Form1()
         {
             InitializeComponent();
-            cn = new Datos.Conexion();
-            
-        }
-
-        private void btnConectar_Click(object sender, EventArgs e)
-        {
-            cn.Conectar("MIZUKI", "", "");
-            MessageBox.Show("Conexion exitosa");
         }
     }
 }
