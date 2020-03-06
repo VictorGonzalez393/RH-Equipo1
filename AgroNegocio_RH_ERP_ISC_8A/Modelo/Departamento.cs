d775eb26a2059b33cd2dee5036164c19c24ca7bc
maritzatp <60710504+maritzatp@users.noreply.github.com>
2020-03-05 21:34:24 -0600

    DAO y Modelo
    Creacion de las clases

---
+++ AgroNegocio_RH_ERP_ISC_8A/Modelo/Departamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroNegocio_RH_ERP_ISC_8A.Modelo
+{
+    class Departamento
+    {
+        public int idDepto { get; set; }
+        public string Nombre { get; set; }
+        public char Estatus { get; set; }
+
+        public Departamento(int id, string nombre, char estatus)
+        {
+            this.idDepto = id;
+            this.Nombre = nombre;
+            this.Estatus = estatus;
+
+        }
+    }
+}
