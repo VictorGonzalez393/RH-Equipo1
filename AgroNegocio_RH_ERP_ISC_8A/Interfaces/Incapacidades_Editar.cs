21752936f7a36047c026af03ff541432ed2b5f2f
Liz <lvilla415@accitesz.com>
2020-03-26 15:41:04 -0600

    Versión 1 diseño interfaz de incapacidades
    Versión 1 del diseño de las interfaces de incapacidad

---
+++ AgroNegocio_RH_ERP_ISC_8A/Interfaces/Incapacidades_Editar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgroNegocio_RH_ERP_ISC_8A.Interfaces
+{
+    public partial class Incapacidades_Editar : Form
+    {
+        public Incapacidades_Editar()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
