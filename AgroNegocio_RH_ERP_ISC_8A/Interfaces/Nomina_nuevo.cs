01ad3b94400d6c7bbf21af9695aa1d0feff8c005
Liz <lvilla415@accitesz.com>
2020-05-02 17:36:37 -0500

    Crear interfaz nueva nomina

---
+++ AgroNegocio_RH_ERP_ISC_8A/Interfaces/Nomina_nuevo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgroNegocio_RH_ERP_ISC_8A.Interfaces
+{
+    public partial class Nomina_nuevo : Form
+    {
+        public Nomina_nuevo()
+        {
+            InitializeComponent();
+        }
+
+        private void textTotalD_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nomina_nuevo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalP_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void atrásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void IniciotoolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Principal p = new Principal();
+            p.ShowDialog();
+            this.Close();
+        }
+    }
+}
