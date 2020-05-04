9ca35326bb70182fdc5445327c27935fe2393e40
VictorGonzalez393 <60683918+VictorGonzalez393@users.noreply.github.com>
2020-05-04 13:32:24 -0500

    Avance

---
+++ AgroNegocio_RH_ERP_ISC_8A/Modelo/NominasPercepcion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroNegocio_RH_ERP_ISC_8A.Modelo
+{
+    class NominasPercepcion
+    {
+        public int IdNomina { get; set; }
+        public int IdPercepcion { get; set; }
+        public double Importe { get; set; }
+        public NominasPercepcion(int idNomina, int idPercepcion, double importe)
+        {
+            IdNomina = idNomina;
+            IdPercepcion = idPercepcion;
+            Importe = importe;
+        }
+    }
+}
